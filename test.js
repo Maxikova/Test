8ad78c88ca8a25f231699bc88b74aa07cb0c37ef
mkovalow <maxi.kovalow@gmail.com>
2024-09-08 20:50:21 -0300

    Añado src

--- test.js
+++ test.js
@@ -23,7 +23,7 @@ test("GET / - Verificar que el servidor esté corriendo y responda con el mensaj
 
 const test = require('ava');
 const request = require('supertest');
-const { app } = require('./app');  // Solo requerimos app
+const { app } = require('./src/app');  // Solo requerimos app
 
 // Verificar que el servidor esté ejecutándose y responda "Server is running"
 test("GET / - Verificar que el servidor esté corriendo y responda con el mensaje 'Server is running'", async t => {
