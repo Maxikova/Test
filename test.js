e723dcb7e128f92f18f7c4341f3448b25e0b6225
mkovalow <maxi.kovalow@gmail.com>
2024-09-08 22:37:04 -0300

    Push

--- test.js
+++ test.js
@@ -1,37 +1,16 @@
-/* const test = require('ava');
+const test = require('ava');
 const request = require('supertest');
-const { app, closeServer } = require('./app');
+const server = require('./index');
 
 // Cerrar el servidor después de las pruebas
 test.after.always(async t => {
-  closeServer();  // Esta función no hará process.exit() ahora
+  server.close();  // Cerramos el servidor después de cada test
 });
 
-// Verificar que el servidor esté ejecutándose y responda "Server is running"
-test("GET / - Verificar que el servidor esté corriendo y responda con el mensaje 'Server is running'", async t => {
-  const response = await request(app).get('/');
-  
-  // Verificar que el código de estado sea 200
+// Prueba para verificar que el servidor responda correctamente
+test('GET / - Verificar que el servidor esté corriendo y responda con el mensaje "Server is running"', async t => {
+  const response = await request(server).get('/');
   t.is(response.status, 200);
-  
-  // Verificar que el cuerpo de la respuesta sea "Server is running"
-  t.is(response.text, 'Server is running');
-}); */
-
-
-
-
-const test = require('ava');
-const request = require('supertest');
-const { app } = require('./src/app');  // Solo requerimos app
-
-// Verificar que el servidor esté ejecutándose y responda "Server is running"
-test("GET / - Verificar que el servidor esté corriendo y responda con el mensaje 'Server is running'", async t => {
-  const response = await request(app).get('/');
-  
-  // Verificar que el código de estado sea 200
-  t.is(response.status, 200);
-  
-  // Verificar que el cuerpo de la respuesta sea "Server is running"
   t.is(response.text, 'Server is running');
 });
+
