e8978a130f0848daa8cff24e9c300536d829d0b7
mkovalow <maxi.kovalow@gmail.com>
2024-09-08 22:25:18 -0300

    Prueba3

--- src/routes/index.js
+++
@@ -1,6 +0,0 @@
-const {Router} = require('express');
-const router = Router(); // Es para las rutas
-
-router.get('/' , (req,res) => res.json({message : 'Hello world'}));
-
-module.exports = router; 
