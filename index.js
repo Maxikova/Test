faaedf77e1f58ec7d7b05d9a474151b2279eb48f
mkovalow <maxi.kovalow@gmail.com>
2024-09-27 19:43:47 -0300

    Commit3

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+
+// Ruta raíz
+app.get('/', (req, res) => {
+  res.status(200).send('Server is running');
+});
+
+// Iniciar el servidor
+const server = app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+  console.log('Server is running ofsddfsn port 300022');
+});
+  
+
+// Exportar el servidor para las pruebas
+module.exports = server;
