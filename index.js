31c2499a1b3cd0ec921478612790a6c9993a7084
mkovalow <maxi.kovalow@gmail.com>
2024-09-27 19:36:19 -0300

    Commit

--- index.js
+++ index.js
@@ -9,7 +9,9 @@ app.get('/', (req, res) => {
 // Iniciar el servidor
 const server = app.listen(3000, () => {
   console.log('Server is running on port 3000');
+  console.log('Server is running on port 300022');
 });
+  
 
 // Exportar el servidor para las pruebas
 module.exports = server;
